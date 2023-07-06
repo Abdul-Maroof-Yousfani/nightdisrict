c73dfdd60ece941c1d0f5a26beb1b3aa3ab4d416
Abdul-Maroof-Yousfani <91761450+Abdul-Maroof-Yousfani@users.noreply.github.com>
2023-07-06 22:36:57 +0500

    Update inapp.js

--- routes/inapp.js
+++ routes/inapp.js
@@ -2,7 +2,7 @@ import express from 'express';
 import inApp from '../controllers/inApp.js';
 const router = express.Router();
 
-
+// 
 router.post("/acknowledge",inApp.acknowledge);
 router.get("/confirm",inApp.confirm);
 
