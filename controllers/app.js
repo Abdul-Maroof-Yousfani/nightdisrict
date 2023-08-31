557d41ebf93ab8837d9b52fe81067b238fe165ee
Abdul-Maroof-Yousfani <91761450+Abdul-Maroof-Yousfani@users.noreply.github.com>
2023-08-31 15:37:03 +0500

    Code Updated!

--- controllers/app.js
+++ controllers/app.js
@@ -30,11 +30,11 @@ const index = async(req,res) =>
 
   
         // recent Orders
-        let orders = await order.find({customer : req.user._id}).lean().limit(3)
-        orders = await Promise.all(orders.map( async (e) =>{
-            // return helpers.getOrderById(e);
-            return await helpers.getItems(e)
-        }))
+        // let orders = await order.find({customer : req.user._id}).lean().limit(3)
+        // orders = await Promise.all(orders.map( async (e) =>{
+        //     // return helpers.getOrderById(e);
+        //     return await helpers.getItems(e)
+        // }))
 
         // get order items only
 
