91f818e685de2aa5cff5a39d6958494a1448cd5c
Abdul-Maroof-Yousfani <91761450+Abdul-Maroof-Yousfani@users.noreply.github.com>
2023-11-13 01:57:05 -0800

    Code updated!

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const app = express();
 app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.json());
-// app.use(fileUpload());
+app.use(fileUpload());
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, 'web')));
 app.use(express.static(path.join(__dirname, 'pdf')));
