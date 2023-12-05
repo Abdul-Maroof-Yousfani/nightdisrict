df171b150105f636f8d32e708b52a7813fdea9e1
Abdul-Maroof-Yousfani <91761450+Abdul-Maroof-Yousfani@users.noreply.github.com>
2023-12-05 12:57:10 +0500

    code updated!

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -7,7 +7,7 @@ const admin = async(req,res,next) =>
  
     try
     {   
-    
+        //dwdsd
         const user = await User.findOne({email:req.user.email});
         const checkRole = await helper.checkRole(user.role);
         if(!checkRole) return res.json({error:"Invalid Role"})
